1d344c36f6cadb85e7ae0a5c65ddb241ac41e7bf
Julian Arce <juarce@itba.edu.ar>
2022-09-27 18:51:09 -0300

    feat: first person camera

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ function animate(currentTime) {
 	 	camera.position.x = cameraOffset.x;
 	 	camera.position.y = cameraOffset.y;
 	 	camera.position.z = cameraOffset.z;
-	 	camera.lookAt(car.position.x, car.position.y, car.position.z);
+	 	camera.lookAt(car.position.x, car.position.y + fp ? 6 : 0, car.position.z);
 	}
 	renderer.render(scene, camera);
 }
@@ -136,8 +136,7 @@ function onDocumentKeyDown(event) {
 		case KEY_CODES.four:
 			orbital = false;
 			fp = true;
-			relativeCameraOffset = new Vector3(0,15,-25);
-			// camera.position.set(car.position.x, car.position.y, car.position.z);
+			relativeCameraOffset = new Vector3(0,6,-2);
 			break;
 		case KEY_CODES.five:
 			orbital = false;
@@ -150,7 +149,7 @@ function onDocumentKeyDown(event) {
 			relativeCameraOffset = new Vector3(25,15,0);
 			break;
 
-	}
+		}
 }
 
 document.addEventListener('keyup', onDocumentKeyUp, false);
